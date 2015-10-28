8b85632e5fb3da77b51ea8f8ba056a4a65f75336
Paramvir Dehal <psdehal@lbl.gov>
2015-10-28 16:00:56 -0700

    initial commit

---
+++ bin/run_singleRepoTestpsd_async_job.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PYTHONPATH=/Users/paramvirdehal/KBase/singleRepoTestpsd/lib:$PATH:$PYTHONPATH
+python /Users/paramvirdehal/KBase/singleRepoTestpsd/lib/singleRepoTestpsd/singleRepoTestpsdServer.py $1 $2 $3
